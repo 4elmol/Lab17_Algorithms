d54c4941c0dd5d101ac358446684c112914c03ff
s0m1k111 <spavel1207@gmail.com>
2026-03-13 16:49:57 +0300

    Step 7 arrays

---
+++ code/Step7_arrayMax/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] a = { 5, 4, 5, 3, 4, 5, 4, 3 };
+int max = a[0];
+int i = 1;
+
+while (i < a.Length)
+{
+    if (a[i] > max)
+    {
+        max = a[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
